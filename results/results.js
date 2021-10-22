18799cb734040d74e43770f4e6c3385d0b2c81c2
mira-kine <mirak0419@gmail.com>
2021-10-22 09:44:33 -0700

    started chart.js on results

--- results/results.js
+++ results/results.js
@@ -8,18 +8,33 @@ const mainPage = document.getElementById('main-page');
 
 for (let item of pokedex){
     const caughtPokemon = findById(item.id, pokemons);
+    const mainContainer = document.createElement('div');
+    mainContainer.classList.add('main-container');
     const div = document.createElement('div');
+    div.classList.add('results-container');
     const img = document.createElement('img');
     img.src = caughtPokemon.url_image;
     const header = document.createElement('h2');
     header.textContent = caughtPokemon.pokemon;
+    const p = document.createElement('p');
     const resultsSpan1 = document.createElement('span');
     resultsSpan1.textContent = `Encountered: ${item.encountered}`;
     const resultsSpan2 = document.createElement('span');
     resultsSpan2.textContent = `Caught: ${item.caught}`;
-
-    div.append(header, img, resultsSpan1, resultsSpan2);
-    mainPage.append(div);
+    p.append(resultsSpan1, resultsSpan2);
+    p.classList.add('results-content');
+    div.append(img, p);
+    mainContainer.append(header, div);
+    mainPage.append(mainContainer);
 }
+
+// const pokemonType = pokedex.map((item)=> {
+//     const pokemon = findById(item.id, pokemons);
+//     return pokemon.pokemonType;
+// });
+
+// let resultsChart = document.getElementById('results-chart').getContext('2d');
+
+
 
 
