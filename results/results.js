81e007469b677ad022465d7e3e9cfdb6ace02592
mira-kine <mirak0419@gmail.com>
2021-10-21 17:28:09 -0700

    images and information come up on results page

--- results/results.js
+++ results/results.js
@@ -1,6 +1,25 @@
-// import pokemons from '../pokemon.js';
-// import { getPokedex } from '../storage-utils.js';
+import pokemons from '../pokemon.js';
+import { getPokedex, findById } from '../storage-utils.js';
 
-// const pokedex = getPokedex();
+const pokedex = getPokedex();
+
+const mainPage = document.getElementById('main-page');
+// create loop
+
+for (let item of pokedex){
+    const caughtPokemon = findById(item.id, pokemons);
+    const div = document.createElement('div');
+    const img = document.createElement('img');
+    img.src = caughtPokemon.url_image;
+    const header = document.createElement('h2');
+    header.textContent = caughtPokemon.pokemon;
+    const resultsSpan1 = document.createElement('span');
+    resultsSpan1.textContent = `Encountered: ${item.encountered}`;
+    const resultsSpan2 = document.createElement('span');
+    resultsSpan2.textContent = `Caught: ${item.caught}`;
+
+    div.append(header, img, resultsSpan1, resultsSpan2);
+    mainPage.append(div);
+}
 
 
